1e8e30cf6b95552ec795fb99e1e8f6d08a9eefdb
munyuaDeveloper <munyuapeter06@gmail.com>
2020-03-22 16:06:50 +0300

    This is a portfolio with django backend and a bootstrap template

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/munyua/Desktop/My django projects/django-portfolio/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
